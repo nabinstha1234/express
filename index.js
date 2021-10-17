b5374d8a6f9863e260c1c2ee923aefb2e943441f
Nabin Shrestha <snabin@insightworkshop.io>
2021-10-17 22:26:00 +0545

    feat add project structures

---
+++ index.js
@@ -0,0 +1,47 @@
+var express = require('express');
+
+var app = express();
+var handlebars = require('express3-handlebars')
+        .create({ defaultLayout: 'main' });
+
+var fortunes = [
+        "Conquer your fears or they will conquer you.",
+        "Rivers need springs.",
+        "Do not fear what you don't know.",
+        "You will have a pleasant surprise.",
+        "Whenever possible, keep it simple.",
+];
+
+app.use(express.static(__dirname + '/public'));
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 8000);
+
+app.get('/', function (req, res) {
+        res.render('home')
+});
+
+app.get('/about', function (req, res) {
+        var randomFortune = fortunes[Math.floor(Math.random()* fortunes.length)];
+
+        res.render('about',{fortune: randomFortune})
+});
+
+// custom 404 page
+
+app.use(function (req, res) {
+        res.status(404);
+        res.render('404')
+})
+
+// custom 500 page
+
+app.use(function (err, req, res, next) {
+        res.status(500);
+        res.render('500')
+});
+
+app.listen(app.get('port'), function () {
+        console.log(app.get('port'))
+});
